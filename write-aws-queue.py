ee2cd850eaeb09a7cc52256cd5dbb6519c46beb4
AriPerkkio <t3pear00@students.oamk.fi>
2015-11-25 16:48:21 +0000

    Works

---
+++ write-aws-queue.py
@@ -0,0 +1,21 @@
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+import sys
+import urllib2
+
+response = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+key = response.read()
+access_key_id = key[0:20] 
+secret_access_key = key[21:]
+
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+queue = conn.get_queue(sys.argv[1])
+queue.set_message_class(Message)
+m = Message()
+m.set_body(sys.argv[2])
+queue.write(m)
+print("Message "+sys.argv[2]+" added to queue "+sys.argv[1]+".")
