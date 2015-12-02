be150135a5adecebcbb98b64ec4512ec036d147f
AriPerkkio <t3pear00@students.oamk.fi>
2015-12-02 15:13:47 +0000

    queue.count() works

--- count-aws-msgs.py
+++ count-aws-msgs.py
@@ -14,7 +14,9 @@ secret_access_key = key[21:]
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 
 queue = conn.get_queue(sys.argv[1])
-rs = queue.get_messages()
+count = queue.count()
+
+
 
 print("Number of messages for queue "+sys.argv[1])
-print(len(rs))
+print(count)
