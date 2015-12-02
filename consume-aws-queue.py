4e57b2808ffec497210925c22366275e55c5ef11
AriPerkkio <t3pear00@students.oamk.fi>
2015-12-02 15:32:19 +0000

    Works

---
+++ consume-aws-queue.py
@@ -0,0 +1,24 @@
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+import sys
+import urllib2
+
+response = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+key = response.read()
+access_key_id = key[0:20] 
+secret_access_key = key[21:]
+
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+queue = conn.get_queue(sys.argv[1])
+messages = queue.get_messages()
+if len(messages) > 0:
+	message = messages[0]
+	print("Message read: "+ message.get_body())
+	queue.delete_message(message)
+	print("Message deleted from queue")
+else:
+	print("No messages")
